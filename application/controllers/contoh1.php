ed5acc8ce8959e33a0b2a187677b4f0931486d48
fcv45 <dextrumer@gmail.com>
2022-09-19 21:09:03 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya fikri ardiansyah
+ Saya tingga di daerah Cikarang
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
