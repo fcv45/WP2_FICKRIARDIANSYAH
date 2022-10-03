f81aee68b94849d306358e3dd50e026fa6d4fad7
fcv45 <dextrumer@gmail.com>
2022-10-03 15:04:16 +0700

    views

--- application/controllers/web.php
+++ application/controllers/web.php
@@ -14,4 +14,13 @@ class web extends CI_Controller{
         $this->load->view('v_index',$data);
         $this->load->view('v_footer',$data);
     }
+
+    public function about()
+    {
+        $data['judul'] = "Halaman About"
+        $this->load->view('v_header', $data);
+        $this->load->view('v_about', $data);
+        $this->load->view('v_footer', $data);
+    }
+
 }
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : FICKRI</li>
+        <li>Nama Belakang : ARDIANSYAH</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> Jalan kenangan anggrek 23 / 33</li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type="none">
+    <li>Cibarusah</li>
+</ul>
+
+<h4>Olah Raga Favorit</h4>
+<ul type="none">
+    <li>Cibarusah</li>
+</ul>
+</section>
